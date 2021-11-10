5b056fce42976792fa9cbe4b4bb763783cdf795c
GislanyDias <diasgislany@gmail.com>
2021-11-10 13:46:00 -0200

    teste

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () =>{
     return(
         <footer className = "rodape">
             <p>Desenvolvido por </p>
-            <p><a href="https://www.instagram.com/ge_stefani_/" target="_blank"> GEOVANNA </a> · <a  href="https://www.instagram.com/_gislanydiasf/" target="_blank">GISLANY</a> · <a href="https://www.instagram.com/igorkadson" target="_blank">IGOR</a></p>
+            <p><a href="https://www.instagram.com/ge_stefani_/" target="_blank"> GEOVANA </a> · <a  href="https://www.instagram.com/_gislanydiasf/" target="_blank">GISLANY</a> · <a href="https://www.instagram.com/igorkadson" target="_blank">IGOR</a></p>
         </footer>
     );
 };
